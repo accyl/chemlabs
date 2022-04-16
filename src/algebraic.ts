4cd115e63bce74cc27f261c1193c28b6ac5d59a4
phlaxyr <phlaxyzerobane@gmail.com>
2022-04-16 16:07:15 -0500

    binclearing

--- src/algebraic.ts
+++ src/algebraic.ts
@@ -1,4 +1,8 @@
-"use strict";
+
+/**
+ * Unused test abstractified class for math, algebra.
+ */
+
 // TODO: generalize this.
 // ie. have a function algebra(eqn: string, vari... variables)
 // in general if a provided variable is undefined, assume that it is
@@ -14,37 +18,47 @@
 // meanwhile if we provide all 5 variables,
 // then algebra() would simply plug it into the equation
 // and check for equality within a certain tolerance.
+
 class Operator {
-    constructor(char) {
+    char;
+    private constructor(char: string) {
         this.char = char;
         Operator.oplist.push(this);
     }
+    static oplist: Operator[] = [];
+    private static __ = function () {
+        let ops = ['+', '-', '*', '/', '^'];
+        for (let op of ops) new Operator(op);
+    }();
+
 }
-Operator.oplist = [];
-Operator.__ = function () {
-    let ops = ['+', '-', '*', '/', '^'];
-    for (let op of ops)
-        new Operator(op);
-}();
 class Expression {
-    constructor() {
-        this.tokenlist = [];
-    }
-    pushVar(varname) {
+    tokenlist: Array<string | Operator> = [];
+    pushVar(varname: string) {
         this.tokenlist.push(varname);
     }
-    pushOp(operator) {
+    pushOp(operator: string) {
         this.tokenlist.push(operator);
     }
 }
-function algebraTknr(inp, startidx) {
+
+function algebraTknr(inp: string, startidx: num) {
     for (let i = startidx; i < inp.length; i++) {
+
     }
 }
-function algebra(eqn, ...vari) {
+function algebra(eqn: string, ...vari: num[]) {
+
 }
-class GeneralizedFunction {
-    constructor(pos, vel, acc, n3deriv) {
+
+abstract class GeneralizedFunction {
+    abstract _zeroes(): tup;
+    _x0: tup;
+    _v0: tup;
+    _a0: tup;
+    n3deriv?: tup[];
+    constructor(pos?: [num, num, num], vel?: [num, num, num], acc?: [num, num, num], n3deriv?: [num, num, num][]) {
+
         this._x0 = pos ? pos : this._zeroes().map(x => x + 1);
         this._v0 = vel ? vel : this._zeroes();
         this._a0 = acc ? acc : this._zeroes();
@@ -52,17 +66,17 @@ class GeneralizedFunction {
             this.n3deriv = n3deriv;
         }
     }
-    pos(t) {
+
+    pos(t?: num): tup {
         return this._x0;
     }
-    vel(t) {
+    vel(t?: num): tup {
         return this._v0;
     }
-    acc(t) {
+    acc(t?: num): tup {
         return this._a0;
-    }
-    ;
-    nthderiv(n, t) {
+    };
+    nthderiv(n: number, t: number): tup {
         switch (n) {
             case 0:
                 return this.pos(t);
@@ -71,23 +85,25 @@ class GeneralizedFunction {
             case 2:
                 return this.acc(t);
             default:
-                if (this.n3deriv)
-                    return this.n3deriv[n - 3];
+                if (this.n3deriv) return this.n3deriv[n - 3];
                 return this._zeroes();
         }
     }
-    step(t0, dt) {
+
+    step(t0: num, dt: num) {
         /**
-         * applies euler's method once
+         * applies euler's method once 
          * dt = stepsize
          */
         let dx = this.vel(t0).map(v => v * dt);
         this._x0 = this.pos(t0).map((x, i) => x + dx[i]); // perform a veloity update
+
+
         let dv = this.acc(t0).map(a => a * dt);
         this._v0 = this.vel(t0).map((v, i) => v + dv[i]); // perform a veloity update
         // TODO updates of higher order derivatives
     }
-    eulers(t0, dt, nsteps) {
+    eulers(t0: num, dt: num, nsteps: num) {
         for (let i = 0; i < nsteps; i++) {
             this.step(t0, dt);
             t0 = t0 + dt;
--- src/alias.ts
+++
@@ -1,3 +0,0 @@
-// const $c = chemicals.get;
-
-
