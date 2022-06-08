d0c8f6393b877c406ae603371c9702c25e4446c8
phlaxyr <phlaxyzerobane@gmail.com>
2022-06-07 22:46:19 -0500

    begin inspector

--- bin/dom/inspector.js
+++ bin/dom/inspector.js
@@ -1,16 +1,53 @@
 "use strict";
 /// <reference path="../../node_modules/@types/jquery/index.d.ts" />
-$('#einspector').on('matterCreated', function (e, eventInfo) {
-    // originates from phys() in phys.ts
-    // alert('(notifier1)The value of eventInfo is: ' + eventInfo);
-    console.log('observed creation of ' + eventInfo['matter'].toString());
-});
-$('#einspector').on('substanceCreated', function (e, eventInfo) {
-    // originates from tang() in physvis.ts, which itself calls phys() but also adds it to glob.s
-    let subs = eventInfo['substance'];
-    console.log('observed appendage of ' + subs.toString() + ' to the glob');
-    let globule = document.createElement('div');
-    globule.classList.add('globule');
-    globule.textContent = subs.physhook.label;
-    $('#einspector')[0].append(globule);
-});
+var _blank;
+(function (_blank) {
+    $('#einspector').on('matterCreated', function (e, eventInfo) {
+        // originates from phys() in phys.ts
+        // alert('(notifier1)The value of eventInfo is: ' + eventInfo);
+        console.log('observed creation of ' + eventInfo['matter'].toString());
+    });
+    // TODO
+    // since we are given mixin creation functions for each phase, we can reflexively discover the attributes that each subclass of substance creates
+    // so we can show them in the inspector
+    function showAttributes(subs) {
+        let subs2 = subs;
+        // subs2.type.
+        let attrs = ['mass', 'volume', 'temperature', 'state', 'mol', 'molarMass'];
+        let s = '\n';
+        for (let attr of attrs) {
+            if (attr in subs) {
+                let result = subs[attr];
+                s += `${attr}: ${subs[attr]}\n`;
+            }
+        }
+        return s;
+    }
+    $('#einspector').on('substanceCreated', function (e, eventInfo) {
+        // originates from tang() in physvis.ts, which itself calls phys() but also adds it to glob.s
+        let subs = eventInfo['substance'];
+        console.log('observed appendage of ' + subs.toString() + ' to the glob');
+        let globule = document.createElement('div');
+        globule.classList.add('globule');
+        globule.textContent = subs.physhook.label;
+        $('#einspector')[0].append(globule);
+        // when clicked, show the substance's properties
+        let $globule = $(globule);
+        $globule.on('click', function () {
+            // expand the div
+            $globule.toggleClass('expanded');
+            if ($globule.hasClass('expanded')) {
+                let child = document.createElement('code');
+                child.classList.add('substance-attributes');
+                child.textContent = showAttributes(subs);
+                // add child to $globule
+                $globule.append(child);
+            }
+            else {
+                // remove child from $globule
+                $globule.children('code.substance-attributes').remove();
+            }
+            // show the substance's properties
+        });
+    });
+})(_blank || (_blank = {}));
